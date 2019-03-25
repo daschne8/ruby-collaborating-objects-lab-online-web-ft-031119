a696ea707a4573959fd7b004881ff2969dc16144
Daniel Schneider <daschne8@gmail.com>
2019-03-25 17:07:26 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,6 +8,8 @@ class MP3Importer
     Dir.entries(path).select {|f| f[-4,f.length-1] == ".mp3"}
   end
   def import
-
+    self.files.each do |file|
+      Song.new_by_filename(file)
+    end
   end
 end
