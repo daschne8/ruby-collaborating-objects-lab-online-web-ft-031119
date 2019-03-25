d2f31ff3b655ad9bb80fcec566b400bd7e21d8a1
Daniel Schneider <daschne8@gmail.com>
2019-03-25 15:32:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,16 @@
 class Artist
   attr_accessor :name
-  attr_reader 
+  attr_reader
+  @@all = []
   def initialize(name)
     @name = name
     @songs = []
   end
-  def add_song()
-    
+  def add_song(song)
+    @songs << song
   end
+  def save
+    @@all << self
+  end
+
 end
